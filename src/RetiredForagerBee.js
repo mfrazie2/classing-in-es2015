8c0b02c032618c4fe35e7eae7852d6f591a34d1a
Mike Frazier <michael.s.frazier@gmail.com>
2016-05-06 21:17:11 -0500

    Complete RetiredForagerBee

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,7 +1,15 @@
 import ForagerBee from './ForagerBee';
 
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super()
+    this.age = 40
+    this.job = "gamble"
+    this.canFly = false
+    this.color = "grey"
+  }
+  gamble(treasure) { this.treasureChest.push(treasure) }
+  forage() { return "I am too old, let me play cards instead" }
 };
 
 export default RetiredForagerBee;
